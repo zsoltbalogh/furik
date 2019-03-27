b52ea6a12ba15ad695d412b8c88f9767750610e1
Zsolt Balogh <zsolt.balogh@liferay.com>
2019-03-27 21:33:03 +0100

    #4 Amount is processed and passed to the payment site

--- wordpress/furik-plugin/furik/furik.php
+++ wordpress/furik-plugin/furik/furik.php
@@ -11,10 +11,19 @@ function furik_form_func( $atts ) {
     ), $atts );
     $r = "<form method=\"POST\" action=\"".$_SERVER['REQUEST_URI']."\">";
     $r .= "<input type=\"hidden\" name=\"furik_action\" value=\"redirect\" />";
+
     $r .= "<div class=\"form-field form-required\">";
     $r .= "<label for=\"furik_form_email\">E-mailcímed:</label>";
     $r .= "<input type=\"text\" name=\"furik_form_email\" id=\"furik_form_email\" />";
     $r .= "</div>";
+
+    $r .= "<br />";
+
+    $r .= "<div class=\"form-field form-required\">";
+    $r .= "<label for=\"furik_form_email\">Az adomány összege:</label>";
+    $r .= "<input type=\"text\" name=\"furik_form_amount\" id=\"furik_form_amount\" value=\"" . $atts['amount'] ."\"/>";
+    $r .= "</div>";
+
     $r .= "<br />";
     $r .= "<p class=\"submit\"><input type=\"submit\" class=\"button button-primary\" value=\"Online támogatás\" /></p>";
     $r .= "</form>";
@@ -32,6 +41,8 @@ function furik_redirect() {
 	require "config.php";
 	require_once 'patched_SimplePayment.class.php';
 
+	$amount = is_numeric($_POST['furik_form_amount']) && $_POST['furik_form_amount'] > 0 ? $_POST['furik_form_amount'] : die("Error: amount is not a number.");
+
 	$orderCurrency = 'HUF';
 	$testOrderId = str_replace(array('.', ':'), "", $_SERVER['SERVER_ADDR']) . @date("U", time()) . rand(1000, 9999);
 	$lu = new SimpleLiveUpdate($config, $orderCurrency);
@@ -41,7 +52,7 @@ function furik_redirect() {
 	    'name' => 'Adomány',
 	    'code' => 'sku0001',
 	    'info' => 'A házra',
-	    'price' => 1207,
+	    'price' => $amount,
 	    'vat' => 0,
 	    'qty' => 1
 	));
