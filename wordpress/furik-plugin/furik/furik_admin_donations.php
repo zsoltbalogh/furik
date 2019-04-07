8750b4b67019d1e364fa8c103b093687e2cef6f5
Zsolt Balogh <zsolt.balogh@liferay.com>
2019-04-07 11:31:52 +0200

    Source formatting, no logic changes

--- wordpress/furik-plugin/furik/furik_admin_donations.php
+++ wordpress/furik-plugin/furik/furik_admin_donations.php
@@ -7,13 +7,13 @@ class Donations_List extends WP_List_Table {
 
 	public function __construct() {
 		parent::__construct( [
-			'singular' => __( 'Donation', 'sp' ),
-			'plural'   => __( 'Donations', 'sp' ),
+			'singular' => __('Donation', 'furik'),
+			'plural'   => __('Donations', 'furik'),
 			'ajax'     => false
 		] );
 	}
 
-	public function column_default( $item, $column_name ) {
+	public function column_default($item, $column_name) {
 		return $item[$column_name];
 	}
 
@@ -43,7 +43,7 @@ class Donations_List extends WP_List_Table {
 
 	}
 
-	public static function get_donations( $per_page = 5, $page_number = 1 ) {
+	public static function get_donations($per_page = 5, $page_number = 1) {
 		global $wpdb;
 
 		$sql = "SELECT
@@ -54,13 +54,13 @@ class Donations_List extends WP_List_Table {
 				{$wpdb->prefix}furik_transactions
 				LEFT OUTER JOIN {$wpdb->prefix}posts campaigns ON ({$wpdb->prefix}furik_transactions.campaign=campaigns.ID)
 				LEFT OUTER JOIN {$wpdb->prefix}posts parentcampaigns ON (campaigns.post_parent=parentcampaigns.ID)";
-		if ( ! empty( $_REQUEST['orderby'] ) ) {
-			$sql .= ' ORDER BY ' . esc_sql( $_REQUEST['orderby'] );
-			$sql .= ! empty( $_REQUEST['order'] ) ? ' ' . esc_sql( $_REQUEST['order'] ) : ' ASC';
+		if (!empty($_REQUEST['orderby'])) {
+			$sql .= ' ORDER BY ' . esc_sql($_REQUEST['orderby']);
+			$sql .= ! empty($_REQUEST['order']) ? ' ' . esc_sql($_REQUEST['order']) : ' ASC';
 		}
 		$sql .= " LIMIT $per_page";
-		$sql .= ' OFFSET ' . ( $page_number - 1 ) * $per_page;
-		$result = $wpdb->get_results( $sql, 'ARRAY_A' );
+		$sql .= ' OFFSET ' . ($page_number - 1) * $per_page;
+		$result = $wpdb->get_results($sql, 'ARRAY_A');
 		return $result;
 	}
 
@@ -69,21 +69,21 @@ class Donations_List extends WP_List_Table {
 
 		$sql = "SELECT COUNT(*) FROM {$wpdb->prefix}furik_transactions";
 
-		return $wpdb->get_var( $sql );
+		return $wpdb->get_var($sql);
 	}
 
 	public function no_items() {
-		_e( 'No donations avaliable.', 'sp' );
+		_e('No donations avaliable.', 'furik');
 	}
 
 	function get_columns() {
 		$columns = [
-			'name' => __( 'Name', 'sp' ),
-			'email' => __('E-mail', 'sp'),
-		    'amount' => __( 'Amount', 'sp' ),
+			'name' => __('Name', 'furik'),
+			'email' => __('E-mail', 'furik'),
+		    'amount' => __('Amount', 'furik'),
 		    'campaign_name' => __('Campaign', 'furik'),
 		    'time' => __('Time', 'sp'),
-		    'transaction_status' => __('Status', 'sp')
+		    'transaction_status' => __('Status', 'furik')
 		];
 
 		return $columns;
@@ -91,9 +91,9 @@ class Donations_List extends WP_List_Table {
 
 	public function get_sortable_columns() {
 		$sortable_columns = array(
-			'name' => array( 'name', false ),
-			'email' => array( 'email', false ),
-			'amount' => array( 'amount', false ),
+			'name' => array('name', false),
+			'email' => array('email', false),
+			'amount' => array('amount', false),
 			'campaign_name' => array('campaign_name', false),
 			'time' => array('time', true),
 			'transaction_status' => array('transaction_status', false)
@@ -103,13 +103,13 @@ class Donations_List extends WP_List_Table {
 	}
 
 	public function prepare_items() {
-		$per_page     = $this->get_items_per_page('donations_per_page', 20);
+		$per_page = $this->get_items_per_page('donations_per_page', 20);
 		$current_page = $this->get_pagenum();
-		$total_items  = self::record_count();
+		$total_items = self::record_count();
 
 		$this->set_pagination_args( [
 		    'total_items' => $total_items,
-		    'per_page'    => $per_page
+		    'per_page' => $per_page
 		] );
 
 		$this->items = self::get_donations($per_page, $current_page);
@@ -122,11 +122,11 @@ class Donations_List_Plugin {
 	public $donations_obj;
 
 	public function __construct() {
-		add_filter( 'set-screen-option', [ __CLASS__, 'set_screen' ], 10, 3 );
-		add_action( 'admin_menu', [ $this, 'plugin_menu' ] );
+		add_filter('set-screen-option', [ __CLASS__, 'set_screen' ], 10, 3);
+		add_action('admin_menu', [$this, 'plugin_menu']);
 	}
 
-	public static function set_screen( $status, $option, $value ) {
+	public static function set_screen($status, $option, $value) {
 		return $value;
 	}
 
@@ -136,21 +136,21 @@ class Donations_List_Plugin {
 			'Donations',
 			'manage_options',
 			'wp_list_table_class',
-			[ $this, 'donations_list_page' ],
+			[$this, 'donations_list_page'],
 			'dashicons-chart-line'
 		);
-		add_action( "load-$hook", [ $this, 'screen_option' ] );
+		add_action("load-$hook", [$this, 'screen_option']);
 	}
 
 	public function screen_option() {
 		$option = 'per_page';
 		$args   = [
-			'label'   => 'Donations',
+			'label' => 'Donations',
 			'default' => 20,
-			'option'  => 'donations_per_page'
+			'option' => 'donations_per_page'
 		];
 
-		add_screen_option( $option, $args );
+		add_screen_option($option, $args);
 
 		$this->donations_obj = new Donations_List();
 	}
@@ -178,7 +178,7 @@ class Donations_List_Plugin {
 	}
 
 	public static function get_instance() {
-		if ( ! isset( self::$instance ) ) {
+		if (!isset(self::$instance)) {
 			self::$instance = new self();
 		}
 
