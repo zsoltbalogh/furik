a32b6b4c75fef3c4f2ebef243d2d9202e842be4b
Zsolt Balogh <zsolt.balogh@liferay.com>
2019-04-10 21:42:47 +0200

    Fix issue with https links

--- wordpress/furik-plugin/furik/furik_helper.php
+++ wordpress/furik-plugin/furik/furik_helper.php
@@ -1,7 +1,7 @@
 <?php
 function furik_url($url, $parameters = array()) {
 	global $furik_homepage_https, $furik_homepage_url;
-	$url = $furik_homepage_https ? "https" : "http" . "://" . $furik_homepage_url . $url;
+	$url = ($furik_homepage_https ? "https" : "http") . "://" . $furik_homepage_url . $url;
 
 	if (count($parameters)) {
 		foreach ($parameters as $key => $value) {
