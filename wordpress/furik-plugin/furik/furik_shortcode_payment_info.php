75a2940f04d5b73770d8b70b6970527500304fce
Zsolt Balogh <zsolt.balogh@liferay.com>
2019-04-14 14:18:45 +0200

    Display order ref

--- wordpress/furik-plugin/furik/furik_shortcode_payment_info.php
+++ wordpress/furik-plugin/furik/furik_shortcode_payment_info.php
@@ -4,6 +4,9 @@
  */
 function furik_shortcode_payment_info($atts) {
 	$s = "";
+	if ($_REQUEST['furik_order_ref']) {
+		$s .= __('Order reference', 'furik') . ': ' . $_REQUEST['furik_order_ref'] . '<br />';
+	}
 	$s .= __('Date', 'furik') . ': ' . date("Y-m-d H:i:s");
 	return $s;
 }
