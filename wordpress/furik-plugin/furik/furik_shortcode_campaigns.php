2f7b880b55a253bb1eaceb4303735d021cbe46f1
Zsolt Balogh <zsolt.balogh@liferay.com>
2019-11-11 20:18:27 +0100

    Making campaign listing smarter

--- wordpress/furik-plugin/furik/furik_shortcode_campaigns.php
+++ wordpress/furik-plugin/furik/furik_shortcode_campaigns.php
@@ -3,14 +3,36 @@
  * WordPress shortcode: [furik_campaigns]: lists all child campaigns
  */
 function furik_shortcode_campaigns($atts) {
-    $post = get_post();
-    $campaigns = get_posts(['post_parent' => $post->ID, 'post_type' => 'campaign', 'numberposts' => 100]);
+	$a = shortcode_atts( array(
+	   'show' => 'image,title,excerpt,progress'
+	), $atts );
 
-    foreach ($campaigns as $campaign) {
-		$r .= "<a href=\"".$campaign->guid."\">".esc_html($campaign->post_title)."</a><br />";
-    }
+	$post = get_post();
+	$campaigns = get_posts(['post_parent' => $post->ID, 'post_type' => 'campaign', 'numberposts' => 100]);
+	$show = explode(",", $a['show']);
 
-    return $r;
+	foreach ($campaigns as $campaign) {
+		$r .= "<div class=\"sub-campaign-listing\">";
+		foreach ($show as $field) {
+			switch ($field) {
+				case "image":
+					$meta = get_post_custom($campaign->ID);
+					$r .= "<a href=\"".$campaign->guid."\"><img src=\"" . $meta['IMAGE'][0] . "\" class=\"sub-campaign-image\" alt=\"" . esc_html($campaign->post_title) . "\"/></a>";
+					break;
+				case "title":
+					$r .= "<div class=\"sub-campaign-title\"><a href=\"".$campaign->guid."\">".esc_html($campaign->post_title)."</a></div>";
+					break;
+				case "excerpt":
+					$r .= "<div class=\"sub-campaign-title\">".esc_html($campaign->post_excerpt)."</div>";
+					break;
+				default:
+					$r .= __('Unknown field: ', 'furik') . $field;
+			}
+		}
+		$r .= "</div>";
+	}
+
+	return $r;
 }
 
 add_shortcode('furik_campaigns', 'furik_shortcode_campaigns');
