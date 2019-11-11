f717d06efcbc7994554b48729638214753ed152e
Zsolt Balogh <zsolt.balogh@liferay.com>
2019-11-11 18:48:25 +0100

    CSS changes to support themes better

--- wordpress/furik-plugin/furik/furik_shortcode_donate_form.php
+++ wordpress/furik-plugin/furik/furik_shortcode_donate_form.php
@@ -31,62 +31,64 @@ function furik_shortcode_donate_form( $atts ) {
     $r .= "<input type=\"hidden\" name=\"furik_campaign\" value=\"$campaign_id\" />";
 
 
-    $r .= "<div class=\"form-field form-required\">";
+    $r .= "<div class=\"form-field form-group form-required\">";
     $r .= "<label for=\"furik_form_campaign\">".__('Supported cause', 'furik').":</label>";
-    $r .= "<input type=\"text\" id=\"furik_form_campaign\" disabled=\"1\" value=\"$campaign\"/>";
+    $r .= "<input type=\"text\" id=\"furik_form_campaign\" class=\"form-control\" disabled=\"1\" value=\"" . htmlspecialchars($campaign) . "\"/>";
     $r .= "</div>";
 
     $r .= "<br />";
 
-    $r .= "<div class=\"form-field form-required\">";
+    $r .= "<div class=\"form-field form-group form-required\">";
     $r .= "<label for=\"furik_form_name\">".__('Your name', 'furik').":</label>";
-    $r .= "<input type=\"text\" name=\"furik_form_name\" id=\"furik_form_name\" required=\"1\"/>";
+    $r .= "<input type=\"text\" name=\"furik_form_name\" id=\"furik_form_name\" class=\"form-control\" required=\"1\"/>";
     $r .= "</div>";
 
-    $r .= "<div class=\"form-field\">";
-    $r .= "<label for=\"furik_form_anon\"><input type=\"checkbox\" name=\"furik_form_anon\" id=\"furik_form_anon\">".__('Anonymous donation', 'furik')."</label>";
+    $r .= "<div class=\"form-field form-check\">";
+    $r .= "<label for=\"furik_form_anon\" class=\"form-check-label\"><input type=\"checkbox\"  class=\"form-check-input\" name=\"furik_form_anon\" id=\"furik_form_anon\"> ".__('Anonymous donation', 'furik')."</label>";
     $r .= "</div>";
 
     $r .= "<br />";
 
-    $r .= "<div class=\"form-field form-required\">";
+    $r .= "<div class=\"form-field form-group form-required\">";
     $r .= "<label for=\"furik_form_email\">".__('E-mail address', 'furik').":</label>";
-    $r .= "<input type=\"email\" name=\"furik_form_email\" id=\"furik_form_email\" required=\"1\" />";
+    $r .= "<input type=\"email\" class=\"form-control\" name=\"furik_form_email\" id=\"furik_form_email\" required=\"1\" />";
     $r .= "</div>";
 
     $r .= "<br />";
 
-    $r .= "<div class=\"form-field form-required\">";
+    $r .= "<div class=\"form-field form-group form-required\">";
     $r .= "<label for=\"furik_form_email\">".__('Donation amount', 'furik')." (Forint):</label>";
-    $r .= "<input type=\"number\" name=\"furik_form_amount\" id=\"furik_form_amount\" value=\"$amount\" required=\"1\" />";
+    $r .= "<input type=\"number\" class=\"form-control\" name=\"furik_form_amount\" id=\"furik_form_amount\" value=\"$amount\" required=\"1\" />";
     $r .= "</div>";
 
     $r .= "<br />";
 
     if (!$a['skip_message']) {
-        $r .= "<div class=\"form-field\">";
+        $r .= "<div class=\"form-field form-group\">";
         $r .= "<label for=\"furik_form_message\">".__('Message', 'furik').":</label>";
-        $r .= "<textarea name=\"furik_form_message\" id=\"furik_form_message\"></textarea>";
+        $r .= "<textarea class=\"form-control\" name=\"furik_form_message\" id=\"furik_form_message\"></textarea>";
         $r .= "</div>";
 
         $r .= "<br />";
     }
 
-    $r .= "<div class=\"form-field\">";
-    $r .= __('Type of donation', 'furik') . ": <br />";
-    $r .= "<label for=\"furik_form_type_0\"><input type=\"radio\" id=\"furik_form_type_0\" name=\"furik_form_type\" value=\"0\" checked=\"1\">".__('Online payment', 'furik')."</label>";
-    $r .= "<label for=\"furik_form_type_1\"><input type=\"radio\" id=\"furik_form_type_1\" name=\"furik_form_type\" value=\"1\">".__('Bank transfer', 'furik')."</label>";
-    $r .= "<label for=\"furik_form_type_2\"><input type=\"radio\" id=\"furik_form_type_2\" name=\"furik_form_type\" value=\"2\">".__('Cash donation', 'furik')."</label>";
+    $r .= "<div class=\"form-field form-group\">";
+    $r .= "<label>" . __('Type of donation', 'furik') . "</label>";
+    $r .= "<div>";
+    $r .= "<div class=\"form-check form-check-inline\"><input type=\"radio\" id=\"furik_form_type_0\" class=\"form-check-input\" name=\"furik_form_type\" value=\"0\" checked=\"1\" /><label for=\"furik_form_type_0\" class=\"form-check-label\">".__('Online payment', 'furik')."</label></div>";
+    $r .= "<div class=\"form-check form-check-inline\"><input type=\"radio\" id=\"furik_form_type_1\" class=\"form-check-input\" name=\"furik_form_type\" value=\"1\"><label for=\"furik_form_type_1\" class=\"form-check-label\">".__('Bank transfer', 'furik')."</label></div>";
+    $r .= "<div class=\"form-check form-check-inline\"><input type=\"radio\" id=\"furik_form_type_2\" class=\"form-check-input\" name=\"furik_form_type\" value=\"2\"><label for=\"furik_form_type_2\" class=\"form-check-label\">".__('Cash donation', 'furik')."</label></div>";
+    $r .= "</div>";
     $r .= "</div>";
 
     $r .= "<br />";
 
-    $r .= "<div class=\"form-field\">";
-    $r .= "<label for=\"furik_form_accept\"><input type=\"checkbox\" name=\"furik_form_accept\" id=\"furik_form_accept\" required=\"1\"><a href=\"".furik_url($furik_data_transmission_declaration_url)."\" target=\"_blank\">".__('I accept the data transmission declaration', 'furik')."</a></label>";
+    $r .= "<div class=\"form-field form-check\">";
+    $r .= "<label for=\"furik_form_accept\" class=\"form-check-label\"><input type=\"checkbox\" name=\"furik_form_accept\" id=\"furik_form_accept\" class=\"form-check-input\" required=\"1\"><a href=\"".furik_url($furik_data_transmission_declaration_url)."\" target=\"_blank\">".__('I accept the data transmission declaration', 'furik')."</a></label>";
     $r .= "</div>";
 
     $r .= "<br />";
-    $r .= "<p class=\"submit\"><input type=\"submit\" class=\"button button-primary\" value=\"".__('Donate', 'furik')."\" /></p>";
+    $r .= "<p class=\"submit\"><input type=\"submit\" class=\"button button-primary rounded-xl btn btn-primary\" value=\"".__('Donate', 'furik')."\" /></p>";
     $r .= "</form>";
 
     $r .= "<a href=\"http://simplepartner.hu/PaymentService/Fizetesi_tajekoztato.pdf\" target=\"_blank\"><img src=\"".furik_url("/wp-content/plugins/furik/images/simplepay.png")."\" title=\"SimplePay - Online bankkártyás fizetés\" alt=\"SimplePay vásárlói tájékoztató\"></a>";
