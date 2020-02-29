7933ca1a313b85bfb84720419ee56c6595a6cc30
Zsolt Balogh <zsolt.balogh@liferay.com>
2020-02-29 20:53:36 +0100

    Hiding a required field is not a good idea in general, it's the case here too

--- wordpress/furik-plugin/furik/furik_shortcode_donate_form.php
+++ wordpress/furik-plugin/furik/furik_shortcode_donate_form.php
@@ -85,16 +85,43 @@ function furik_shortcode_donate_form( $atts ) {
     $r .= "<div class=\"form-field form-group furik-payment-method\">";
     // $r .= "<label>" . __('Type of donation', 'furik') . "</label>";
     $r .= "<div>";
-    $r .= "<div class=\"form-check form-check-inline\"><input type=\"radio\" id=\"furik_form_type_0\" class=\"form-check-input\" name=\"furik_form_type\" value=\"0\" checked=\"1\" onChange=\"document.getElementById('furik_form_accept_reg_div').style.display='none'; document.getElementById('furik_form_submit_button').value='" . __('Send', 'furik')."'\" /><label for=\"furik_form_type_0\" class=\"form-check-label\">".__('Online payment', 'furik')."</label></div>";
+    $r .= "<div class=\"form-check form-check-inline\">
+        <input type=\"radio\" id=\"furik_form_type_0\" class=\"form-check-input\" name=\"furik_form_type\" value=\"0\" checked=\"1\"
+            onChange=\"document.getElementById('furik_form_accept_reg_div').style.display='none';
+                document.getElementById('furik_form_submit_button').value='" . __('Send', 'furik')."';
+                document.getElementById('furik_form_accept_reg').required=false\"
+        />
+        <label for=\"furik_form_type_0\" class=\"form-check-label\">".__('Online payment', 'furik')."</label></div>";
 
     if ($a['enable_monthly']) {
-        $r .= "<div class=\"form-check form-check-inline\"><input type=\"radio\" id=\"furik_form_type_1\" class=\"form-check-input\" name=\"furik_form_type\" value=\"3\" onChange=\"document.getElementById('furik_form_accept_reg_div').style.display='block'; document.getElementById('furik_form_submit_button').value='" . __('Donation with card registration', 'furik')."'\"><label for=\"furik_form_type_1\" class=\"form-check-label\">".__('Monthly automatic donation', 'furik'). " <a href=\"" . furik_url($furik_monthly_explanation_url) . "\" target=\"_blank\">". __("What's this?", 'furik')."</a></label></div>";
+        $r .= "<div class=\"form-check form-check-inline\">
+            <input type=\"radio\" id=\"furik_form_type_1\" class=\"form-check-input\" name=\"furik_form_type\" value=\"3\"
+                onChange=\"
+                    document.getElementById('furik_form_accept_reg_div').style.display='block';
+                    document.getElementById('furik_form_submit_button').value='" . __('Donation with card registration', 'furik')."';
+                    document.getElementById('furik_form_accept_reg').required=true\"
+            />
+            <label for=\"furik_form_type_1\" class=\"form-check-label\">".__('Monthly automatic donation', 'furik'). " <a href=\"" . furik_url($furik_monthly_explanation_url) . "\" target=\"_blank\">". __("What's this?", 'furik')."</a></label></div>";
     }
 
-    $r .= "<div class=\"form-check form-check-inline\"><input type=\"radio\" id=\"furik_form_type_1\" class=\"form-check-input\" name=\"furik_form_type\" value=\"1\" onChange=\"document.getElementById('furik_form_accept_reg_div').style.display='none'; document.getElementById('furik_form_submit_button').value='" . __('Send', 'furik')."'\"><label for=\"furik_form_type_1\" class=\"form-check-label\">".__('Bank transfer', 'furik')."</label></div>";
+    $r .= "<div class=\"form-check form-check-inline\">
+        <input type=\"radio\" id=\"furik_form_type_1\" class=\"form-check-input\" name=\"furik_form_type\" value=\"1\"
+            onChange=\"
+                document.getElementById('furik_form_accept_reg_div').style.display='none';
+                document.getElementById('furik_form_submit_button').value='" . __('Send', 'furik')."';
+                document.getElementById('furik_form_accept_reg').required=false\"
+            />
+            <label for=\"furik_form_type_1\" class=\"form-check-label\">".__('Bank transfer', 'furik')."</label></div>";
 
     if ($a['enable_cash']) {
-        $r .= "<div class=\"form-check form-check-inline\"><input type=\"radio\" id=\"furik_form_type_2\" class=\"form-check-input\" name=\"furik_form_type\" value=\"2\" onChange=\"document.getElementById('furik_form_accept_reg_div').style.display='none'; document.getElementById('furik_form_submit_button').value='" . __('Send', 'furik')."'\"><label for=\"furik_form_type_2\" class=\"form-check-label\">".__('Cash donation', 'furik')."</label></div>";
+        $r .= "<div class=\"form-check form-check-inline\">
+            <input type=\"radio\" id=\"furik_form_type_2\" class=\"form-check-input\" name=\"furik_form_type\" value=\"2\"
+                onChange=\"
+                    document.getElementById('furik_form_accept_reg_div').style.display='none';
+                    document.getElementById('furik_form_submit_button').value='" . __('Send', 'furik')."';
+                    document.getElementById('furik_form_accept_reg').required=false\"
+            />
+            <label for=\"furik_form_type_2\" class=\"form-check-label\">".__('Cash donation', 'furik')."</label></div>";
     }
 
     $r .= "</div>";
@@ -107,7 +134,7 @@ function furik_shortcode_donate_form( $atts ) {
     $r .= "</div>";
 
     $r .= "<div class=\"form-field form-check furik-form-accept-reg\" id=\"furik_form_accept_reg_div\" style=\"display: none\">";
-    $r .= "<label for=\"furik_form_accept_reg\" class=\"form-check-label\"><input type=\"checkbox\" name=\"furik_form_accept-reg\" id=\"furik_form_accept_reg\" class=\"form-check-input\" required=\"1\"><a href=\"".furik_url($furik_card_registration_statement_url)."\" target=\"_blank\">".__('I accept the card registration statement', 'furik')."</a></label>";
+    $r .= "<label for=\"furik_form_accept_reg\" class=\"form-check-label\"><input type=\"checkbox\" name=\"furik_form_accept-reg\" id=\"furik_form_accept_reg\" class=\"form-check-input\"><a href=\"".furik_url($furik_card_registration_statement_url)."\" target=\"_blank\">".__('I accept the card registration statement', 'furik')."</a></label>";
     $r .= "</div>";
 
     $r .= "<div class=\"py-4 footer-btns\"><div class=\"submit-btn\">";
