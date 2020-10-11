dc2d7785c83aeaa3805aff1c659f9adb91305f0a
Zsolt Balogh <zsolt.balogh@liferay.com>
2020-10-11 20:19:39 +0200

    Should not show future transactions, they are not useful for admins

--- wordpress/furik-plugin/furik/furik_admin_donations.php
+++ wordpress/furik-plugin/furik/furik_admin_donations.php
@@ -81,7 +81,8 @@ class Donations_List extends WP_List_Table {
 			FROM
 				{$wpdb->prefix}furik_transactions
 				LEFT OUTER JOIN {$wpdb->prefix}posts campaigns ON ({$wpdb->prefix}furik_transactions.campaign=campaigns.ID)
-				LEFT OUTER JOIN {$wpdb->prefix}posts parentcampaigns ON (campaigns.post_parent=parentcampaigns.ID)";
+				LEFT OUTER JOIN {$wpdb->prefix}posts parentcampaigns ON (campaigns.post_parent=parentcampaigns.ID)
+			WHERE transaction_status != ". FURIK_STATUS_FUTURE;
 		if (!empty($_REQUEST['orderby'])) {
 			$sql .= ' ORDER BY ' . esc_sql($_REQUEST['orderby']);
 			$sql .= ! empty($_REQUEST['order']) ? ' ' . esc_sql($_REQUEST['order']) : ' ASC';
